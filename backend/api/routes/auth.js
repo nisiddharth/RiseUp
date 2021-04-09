fb74c8aea7ffad9cc076a953de1e5b8a30cc49f1
Om Singh <omsingh6903@gmail.com>
2021-04-09 22:51:28 +0530

    created user model

---
+++ backend/api/routes/auth.js
@@ -0,0 +1,6 @@
+const { Router } = require('express');
+
+const route = Router();
+
+route.post('/signIn',);
+route.post('/signUp',);
---
+++ backend/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    deviceTokens: [
+        {
+            type: String,
+        }
+    ],
+    encryptedPassword: String,
+    salt: String,
+}, {
+    timestamps: true,
+    toObject: {
+        virtuals: true,
+    },
+    toJSON: {
+        virtuals: true,
+    }
+});
+
+
+UserSchema.virtual('password').set(function (password) {
+    this.salt = uuidv1();
+    this.encryptedPassword = this.securePassword(password);
+});
+
+UserSchema.methods = {
+    authenticate: function (plainPassword) {
+        return this.securePassword(plainPassword) === this.encryptedPassword
+    },
+
+    securePassword: function (plainPassword) {
+        if (!plainPassword) return "";
+        try {
+            return crypto.createHmac('sha256', this.salt)
+                .update(plainPassword)
+                .digest('hex');
+        } catch (err) {
+            return "";
+        }
+    }
+}
+
+module.exports = mongoose.model('user', UserSchema);
