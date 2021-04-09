256c1e26b41bd755009046e6425b1e437f365e99
ashu12chi <ashutosh7234@gmail.com>
2021-04-10 00:12:52 +0530

    TabLayout

--- Android/app/src/main/java/com/npdevs/riseup/Front.java
+++ Android/app/src/main/java/com/npdevs/riseup/Front.java
@@ -1,14 +1,53 @@
 package com.npdevs.riseup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class Front extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem tabItem1,tabItem2,tabItem3;
+    ViewPager viewPager;
+    PageAdaptor pageAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
+
+        tabLayout = findViewById(R.id.tablayout1);
+        tabItem1 = findViewById(R.id.tab1);
+        tabItem2 = findViewById(R.id.tab2);
+        tabItem3 = findViewById(R.id.tab3);
+        viewPager = findViewById(R.id.vpager);
+        pageAdaptor = new PageAdaptor(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdaptor);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 2) {
+                    pageAdaptor.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
---
+++ Android/app/src/main/java/com/npdevs/riseup/PageAdaptor.java
@@ -0,0 +1,36 @@
+package com.npdevs.riseup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdaptor extends FragmentPagerAdapter {
+
+    int tabCount;
+
+    public PageAdaptor(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabCount = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ftab1();
+            case 1:
+                return new ftab2();
+            case 2:
+                return new ftab3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
