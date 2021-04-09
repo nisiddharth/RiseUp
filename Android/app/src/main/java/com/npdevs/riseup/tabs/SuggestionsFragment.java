7f6f6f8af217ab9c1ec76ae46c5a8dd1af816dd1
Nishchal Siddharth Pandey <nisiddharth@gmail.com>
2021-04-10 04:22:23 +0530

    Edited Login, Renamed XMLs

--- Android/app/src/main/java/com/npdevs/riseup/tabs/SuggestionsFragment.java
+++ Android/app/src/main/java/com/npdevs/riseup/tabs/SuggestionsFragment.java
@@ -66,7 +66,7 @@ public class SuggestionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_ftab3, container, false);
+        View view = inflater.inflate(R.layout.fragment_suggestions, container, false);
 
         musicCardView = view.findViewById(R.id.music);
         musicCardView.setOnClickListener(new View.OnClickListener(){
--- Android/app/src/main/java/com/npdevs/riseup/tabs/SummaryFragment.java
+++ Android/app/src/main/java/com/npdevs/riseup/tabs/SummaryFragment.java
@@ -61,6 +61,6 @@ public class SummaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ftab1, container, false);
+        return inflater.inflate(R.layout.fragment_summary, container, false);
     }
 }
